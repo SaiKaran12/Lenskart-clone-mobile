d0fc2179909f851c1be6f1e23c14ddd9894392e6
Karan <bagulkaran847@gmail.com>
2024-09-27 16:25:11 +0530

    Add files via upload

---
+++ lens2.js
@@ -0,0 +1,16 @@
+
+alert("this website is only for mobile view");
+let bu2=document.querySelector(".hlnavv");
+let bu=document.querySelector(".hlnav");
+let tab=document.querySelector(".taab")
+
+
+
+bu.addEventListener("click",()=>{
+    tab.style.height="700px";
+    tab.style.width="80%";
+});
+bu2.addEventListener("click",()=>{
+    tab.style.height="0px";
+    tab.style.width="0%";
+})
